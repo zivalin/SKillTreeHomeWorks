c3e7f038db789482a16b4afc19b12c0419324d29
zivalin <zivalin@gmail.com>
2016-04-13 22:32:49 +0800

    加入第一天回家作業

---
+++ HomeworkDay1/HomeworkDay1/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HomeworkDay1.Models.ViewModels;
+
+namespace HomeworkDay1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Random rnd = new Random();
+            List<MoneyViewModel> listDatas = new List<MoneyViewModel>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                listDatas.Add(
+                    new MoneyViewModel()
+                    {
+                        SR = i,
+                        IO = (i / 2) == 1 ? "支出" : "收入",
+                        CHDT = DateTime.Now.AddDays(rnd.Next(1, 10)),
+                        MNY = rnd.Next(1, 10) * (i+1) * 100
+                    }
+                );
+            }
+            return View(listDatas);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ HomeworkDay1/HomeworkDay1/Models/ViewModels/MoneyViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeworkDay1.Models.ViewModels
+{
+    /// <summary>
+    /// 記帳記錄 
+    /// </summary>
+    public class MoneyViewModel
+    {
+        /// <summary>
+        /// 序號
+        /// </summary>
+        public int SR { get; set; }
+        /// <summary>
+        /// 異動別
+        /// </summary>
+        public string IO { get; set; }
+        /// <summary>
+        /// 異動日期
+        /// </summary>
+        public DateTime CHDT { get; set; }
+        /// <summary>
+        /// 金額
+        /// </summary>
+        public decimal MNY { get; set; }  
+    }
+}
